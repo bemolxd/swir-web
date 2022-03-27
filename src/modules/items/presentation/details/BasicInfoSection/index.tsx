4f23822b49e65b2b5b3a4edc137420e7f121bd1b
bemolx <bartosz.bem@bemideas.pl>
2022-03-27 15:56:53 +0200

    [ItemDetails] dostosowanie widoku pod urządzenia mobilne

--- src/modules/items/presentation/details/BasicInfoSection/index.tsx
+++ src/modules/items/presentation/details/BasicInfoSection/index.tsx
@@ -1,65 +1,33 @@
-import { Heading, HStack, Image, VStack, Divider } from "@chakra-ui/react";
+import { HStack, Image, Divider, VStack } from "@chakra-ui/react";
+import { useCheckMobile } from "components/Layout";
 
 import { Item } from "modules/items/application";
-import { useIntl } from "react-intl";
-
-import { BasicPropertyInfo } from "./BasicPropertyInfo";
+import { BasicInfoContent } from "./BasicInfoContent";
 
 interface IProps {
   details: Item;
 }
 
 export const BasicInfoSection = ({ details }: IProps) => {
-  const { formatMessage } = useIntl();
-  const { vendor, type, category, subcategory, quantity, imageUrl } = details;
+  const isMobile = useCheckMobile();
+
+  if (isMobile) {
+    return (
+      <>
+        <VStack align="flex-start" justify="space-between" w="100%">
+          <Image src={details.imageUrl} w="400px" borderRadius={8} my={1} />
+          <BasicInfoContent details={details} />
+        </VStack>
+        <Divider />
+      </>
+    );
+  }
 
   return (
     <>
       <HStack align="flex-start" justify="space-between" w="100%">
-        <Image src={imageUrl} w="400px" borderRadius={8} my={1} />
-        <VStack maxW="400px" w="100%" align="flex-start">
-          <Heading size="md" fontWeight="400">
-            {formatMessage({
-              id: "ItemDetails.content.BasicInfoSection.header",
-              defaultMessage: "Podstawowe informacje:",
-            })}
-          </Heading>
-          <BasicPropertyInfo
-            property={formatMessage({
-              id: "ItemDetails.content.BasicInfoSection.vendor",
-              defaultMessage: "producent",
-            })}
-            value={vendor}
-          />
-          <BasicPropertyInfo
-            property={formatMessage({
-              id: "ItemDetails.content.BasicInfoSection.type",
-              defaultMessage: "rodzaj",
-            })}
-            value={type}
-          />
-          <BasicPropertyInfo
-            property={formatMessage({
-              id: "ItemDetails.content.BasicInfoSection.category",
-              defaultMessage: "kategoria",
-            })}
-            value={category}
-          />
-          <BasicPropertyInfo
-            property={formatMessage({
-              id: "ItemDetails.content.BasicInfoSection.subcategory",
-              defaultMessage: "podkategoria",
-            })}
-            value={subcategory ?? "---"}
-          />
-          <BasicPropertyInfo
-            property={formatMessage({
-              id: "ItemDetails.content.BasicInfoSection.quantity",
-              defaultMessage: "ilość",
-            })}
-            value={quantity}
-          />
-        </VStack>
+        <Image src={details.imageUrl} w="400px" borderRadius={8} my={1} />
+        <BasicInfoContent details={details} />
       </HStack>
       <Divider />
     </>
