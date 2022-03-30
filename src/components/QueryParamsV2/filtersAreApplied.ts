b70279e8e091cbe050eb733bee32cba4ddf6ec91
bemolx <bartosz.bem@bemideas.pl>
2022-03-30 13:09:30 +0200

    [QueryParamsV2] stworzenie helpera filtersAreApplied

---
+++ src/components/QueryParamsV2/filtersAreApplied.ts
@@ -0,0 +1,7 @@
+import { isEmpty, omit } from "lodash";
+
+import { IQueryParams } from "types";
+
+export const filtersAreApplied = <Params extends IQueryParams>(
+  params: Params
+) => !isEmpty(omit(params, ["limit", "offset"]));
