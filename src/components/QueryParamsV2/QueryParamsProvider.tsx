91b817c48b61cd5d2e62f7bf7fec56780a1f50ea
bemolx <bartosz.bem@bemideas.pl>
2022-03-31 14:35:06 +0200

    [QueryParamsV2] stworzenie providera

---
+++ src/components/QueryParamsV2/QueryParamsProvider.tsx
@@ -0,0 +1,46 @@
+import { createContext, ReactNode, useContext } from "react";
+
+import { IChildrenProp } from "types";
+
+import { Location } from "./LocationManager";
+
+export interface Values {
+  location: Location;
+  history: History;
+}
+
+type History = {
+  push: Function;
+  replace: Function;
+};
+
+interface IProps extends IChildrenProp {
+  location: Location;
+  history: History;
+}
+
+export const QueryParamsContext = createContext<Values | undefined>(undefined);
+
+export const QueryParamsProvider = ({
+  location,
+  history,
+  children,
+}: IProps) => {
+  return (
+    <QueryParamsContext.Provider value={{ history, location }}>
+      {children}
+    </QueryParamsContext.Provider>
+  );
+};
+
+export const useQueryParamsConsumer = (): Values => {
+  const value = useContext(QueryParamsContext);
+
+  if (!value) {
+    throw new Error(
+      "QueryParamsConsumer must be used within QueryParamsProvider"
+    );
+  }
+
+  return value;
+};
