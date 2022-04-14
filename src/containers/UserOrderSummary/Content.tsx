1f8af97968cc880ba3b9b86643259ee97da3bf6f
bemolx <bartosz.bem@bemideas.pl>
2022-04-14 17:45:43 +0200

    [#8][Submit Order] dodanie defaultowych danych formularza

--- src/containers/UserOrderSummary/Content.tsx
+++ src/containers/UserOrderSummary/Content.tsx
@@ -3,8 +3,11 @@ import { Form } from "components/Form";
 
 import { withSuspense } from "components/RemoteData";
 
-import { Order } from "modules/userOrders/application";
-import { useOrderQuery } from "modules/userOrders/infrastructure";
+import { SelectedItem } from "modules/userOrders/application";
+import {
+  useOrderQuery,
+  useSubmitOrderRequest,
+} from "modules/userOrders/infrastructure";
 import {
   DateSection,
   SelectedItems,
@@ -12,21 +15,53 @@ import {
   SendSection,
   SummaryHeader,
   TechSelectSection,
+  useSubmitOrderNotifications,
 } from "modules/userOrders/presentation";
 
 interface IProps {
   orderId: string;
 }
 
+interface FormData {
+  techId: string | null;
+  items: SelectedItem[] | null;
+  senderComment: string | null;
+  dateFrom: string | null;
+  dateTo: string | null;
+}
+
 export const Content = withSuspense(({ orderId }: IProps) => {
   const order = useOrderQuery(orderId);
+  const [submitData] = useSubmitOrderRequest(order?.senderId!, orderId);
+  const { showSuccessNotification, showErrorNotification } =
+    useSubmitOrderNotifications();
+
   const formId = `order.${orderId}.summaryForm`;
 
+  const formData: FormData = {
+    techId: null,
+    items: order?.items ?? null,
+    senderComment: null,
+    dateFrom: null,
+    dateTo: null,
+  };
+
+  const handleOnSubmit = async (data: any) => {
+    try {
+      console.log(data);
+      await submitData(data);
+      showSuccessNotification();
+    } catch (error) {
+      showErrorNotification();
+    }
+  };
+
   return (
-    <Form<Order>
+    <Form<FormData>
       id={formId}
-      onSubmit={(data) => console.log(data)}
-      defaultValues={order}
+      onSubmit={(data) => handleOnSubmit(data)}
+      defaultValues={formData}
+      resetOnSubmit
     >
       <VStack align="flex-start" w="100%">
         <SummaryHeader />
