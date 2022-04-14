d7712aa463b4bda8c6a65f5674b5e6f3ad665ec6
bemolx <bartosz.bem@bemideas.pl>
2022-04-14 13:56:19 +0200

    [Items] zmiana prezentacji filtrów w headerze

--- src/containers/Items/index.tsx
+++ src/containers/Items/index.tsx
@@ -2,11 +2,8 @@ import { useEffect } from "react";
 import { VStack } from "@chakra-ui/react";
 import { isEmpty } from "lodash";
 
-import { Card } from "components/Card";
 import { withErrorBoundary } from "components/ErrorBoundary";
 import { useQueryParams } from "components/QueryParamsV2";
-
-import { FilterSection } from "modules/items/presentation";
 
 import { Content } from "./Content";
 import { Header } from "./Header";
@@ -21,10 +18,7 @@ export const ItemsContainer = withErrorBoundary(() => {
 
   return (
     <VStack w="100%" align="flex-start">
-      <Card maxW="1300px" w="100%">
-        <Header />
-        <FilterSection />
-      </Card>
+      <Header />
       <Content />
     </VStack>
   );
