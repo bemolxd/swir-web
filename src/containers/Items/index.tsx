60877b071991801bac8ef4a6fe41a164a7fb4b17
bemolx <bartosz.bem@bemideas.pl>
2022-04-14 14:07:37 +0200

    [Items] zmiana obsługi dodawania defaultowych paramsów

--- src/containers/Items/index.tsx
+++ src/containers/Items/index.tsx
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
 import { VStack } from "@chakra-ui/react";
 import { isEmpty } from "lodash";
+import { useNavigate } from "react-router";
+import { buildUrl } from "utils";
 
 import { withErrorBoundary } from "components/ErrorBoundary";
 import { useQueryParams } from "components/QueryParamsV2";
@@ -9,10 +11,13 @@ import { Content } from "./Content";
 import { Header } from "./Header";
 
 export const ItemsContainer = withErrorBoundary(() => {
-  const { params, set } = useQueryParams();
+  const { params, url } = useQueryParams();
+  const navigate = useNavigate();
 
   useEffect(() => {
-    if (isEmpty(params)) set({ limit: 10, offset: 0 });
+    if (isEmpty(params)) {
+      navigate(buildUrl(url, { limit: 10, offset: 0 }), { replace: true });
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [params]);
 
