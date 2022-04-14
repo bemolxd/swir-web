a1c63ff3bd97802cfed45a7e62b7ce400d24a617
bemolx <bartosz.bem@bemideas.pl>
2022-04-14 13:56:52 +0200

    [MainRoutes] dostosowanie MainRoutes do react-router v6

--- src/containers/MainRoutes/index.tsx
+++ src/containers/MainRoutes/index.tsx
@@ -1,14 +1,25 @@
-import { Redirect, Route, Switch } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+
+import { Redirect } from "components/Routes";
 
 import { ItemsContainer } from "containers/Items";
 import { ItemDetailsContainer } from "containers/ItemDetails";
+import { UserOrders } from "containers/UserOrders";
+import { UserOrderDetails } from "containers/UserOrderDetails";
+import { UserOrderSummary } from "containers/UserOrderSummary";
 
 export const MainRoutes = () => {
   return (
-    <Switch>
-      <Route path="/sprzet" component={ItemsContainer} exact />
-      <Route path="/sprzet/:itemId" component={ItemDetailsContainer} />
-      <Route render={() => <Redirect to="/sprzet" />} />
-    </Switch>
+    <Routes>
+      <Route path="/sprzet" element={<ItemsContainer />} />
+      <Route path="/sprzet/:itemId" element={<ItemDetailsContainer />} />
+      <Route path="/zgloszenia" element={<UserOrders />} />
+      <Route path="/zgloszenia/:orderId" element={<UserOrderDetails />} />
+      <Route
+        path="/zgloszenia/:orderId/podsumowanie"
+        element={<UserOrderSummary />}
+      />
+      <Route path="*" element={<Redirect to="/sprzet" />} />
+    </Routes>
   );
 };
