4d0146859427b095babf6bec415e8827c1659bd6
bemolxd <bartosz.bem@bemideas.pl>
2022-10-19 14:54:09 +0200

    feat: dodanie przechowywania typu listy w localStorage

--- src/modules/items/presentation/filtering/DisplayTypeButtons.tsx
+++ src/modules/items/presentation/filtering/DisplayTypeButtons.tsx
@@ -1,6 +1,8 @@
 import { ButtonGroup } from "@chakra-ui/react";
 import { MdViewList, MdViewModule } from "react-icons/md";
 import { useIntl } from "react-intl";
+import { useEffect } from "react";
+import { useLocalStorage } from "utils";
 
 import { IconButton } from "components/IconButton";
 import { Separator } from "components/Filters";
@@ -11,7 +13,17 @@ import { useItemsViewHandler } from "modules/items/application";
 export const DisplayTypeButtons = () => {
   const { formatMessage } = useIntl();
   const setView = useItemsViewHandler((handler) => handler.setView);
+  const [value, setValue] = useLocalStorage("grid-layout", false);
   const isMobile = useCheckMobile();
+
+  useEffect(() => {
+    if (!value) {
+      setView("list");
+      return;
+    }
+
+    setView("grid");
+  }, [value, setView]);
 
   if (isMobile) return null;
 
@@ -25,7 +37,10 @@ export const DisplayTypeButtons = () => {
           })}
           icon={<MdViewList />}
           variant="ghost"
-          onClick={() => setView("list")}
+          onClick={() => {
+            setView("list");
+            setValue(false);
+          }}
         />
         <IconButton
           tooltip={formatMessage({
@@ -34,7 +49,10 @@ export const DisplayTypeButtons = () => {
           })}
           icon={<MdViewModule />}
           variant="ghost"
-          onClick={() => setView("grid")}
+          onClick={() => {
+            setView("grid");
+            setValue(true);
+          }}
         />
       </ButtonGroup>
       <Separator />
