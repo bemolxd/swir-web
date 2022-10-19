42ffc13ec7576471c5c2687769ffe0b579262b74
bemolxd <bartosz.bem@bemideas.pl>
2022-10-19 14:27:18 +0200

    fix: rozszerzenie dayjs o duration

--- src/utils/dayjs.ts
+++ src/utils/dayjs.ts
@@ -2,9 +2,11 @@ import dayjs from "dayjs";
 import "dayjs/locale/pl";
 import LocalizedFormat from "dayjs/plugin/localizedFormat";
 import relativeTime from "dayjs/plugin/relativeTime";
+import duration from "dayjs/plugin/duration";
 
 dayjs.locale("pl");
 dayjs.extend(relativeTime);
 dayjs.extend(LocalizedFormat);
+dayjs.extend(duration);
 
 export { dayjs };
