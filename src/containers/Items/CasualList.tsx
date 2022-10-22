1cefe598e2665e2df237f8f3c1d9cdd683083331
bemolxd <bartosz.bem@bemideas.pl>
2022-10-22 16:18:55 +0200

    fix: remove unused hooks from items casual list

--- src/containers/Items/CasualList.tsx
+++ src/containers/Items/CasualList.tsx
@@ -4,16 +4,12 @@ import { withSuspense } from "components/RemoteData";
 
 import { useItemsViewHandler } from "modules/items/application";
 import { useItemsQuery } from "modules/items/infrastructure";
-import { useItemsAvailabilityQuery } from "modules/items/infrastructure/useItemsAvailabilityQuery";
 import { ItemsGrid, ItemsList } from "modules/items/presentation";
 
 export const CasualList = withSuspense(() => {
   const { params } = useQueryParams();
   const items = useItemsQuery(params);
   const view = useItemsViewHandler((handler) => handler.view);
-  console.log(
-    useItemsAvailabilityQuery(items?.collection.map((item) => item.itemId)!)
-  );
 
   return (
     <>
